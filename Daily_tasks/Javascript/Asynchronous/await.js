7964427f9ebf9fb508e841a0c7a206e1bca979e4
SUMEKA  A <54205698+sumeka-it19@users.noreply.github.com>
2023-02-27 06:28:55 +0530

    Update await.js

--- Daily_tasks/Javascript/Asynchronous/await.js
+++ Daily_tasks/Javascript/Asynchronous/await.js
@@ -1,2 +1,51 @@
+//async
+function value(x){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{ resolve(x);},1000);
+    })
+} 
+const a = async function(x){
+ const b = await value(12);
+ const c = await value(30);
+ return x+b+c;
+}
 
+a(10).then((v)=> { console.log(v)});
+
+
+//await
+function res(x) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(x);
+      }, 3000);
+    });
+  }
+  
+  async function f1() {
+    const x = await res(10);
+    console.log(x);
+  }
+  
+  f1();
+
+
+  async function can(name) {
+    console.log(name, "start");
+    await console.log(name, "middle");
+    console.log(name, "end");
+  }
+  
+  can("First");
+  can("Second");
+  
+
+  async function f() {
+    const y = await 20;
+    console.log(y);// 20
+    const obj = {};
+    console.log((await obj) === obj);// true
+  }
+  
+  f();
 
