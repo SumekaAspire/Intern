f9dc032752fd3d8085ba33d5b2b19b532554a0b2
SUMEKA  A <54205698+sumeka-it19@users.noreply.github.com>
2023-02-27 17:53:45 +0530

    Update object.js

--- Daily_tasks/Javascript/Objects/object.js
+++ Daily_tasks/Javascript/Objects/object.js
@@ -51,19 +51,120 @@ val.age = 23;
 val.name[0]= 'sumeka';
 val.subject.sub1='c++';
 console.log(`---------------------`);
+//adding new property
+val.year=2019;
+console.log(val);
+
+val['year']=2022;
+console.log(val);
+console.log(val['year']);
+
+let key= 'age';
+val[key]=25;//updated
+val.key=20;//val.age is not updated
+console.log(val);
 
 
-class valu{
+//anotherway to create object
+let item = new Object();
+item .name='akil'
+item .age=16
+console.log(item);
 
-    
-    6(name){
-        this.name=name;
+let user ={
+    name:'sumeka',
+    age:20,
+    login(){
+        console.log("Hi",this.name);
+        console.log("you are logged in");
+    },
+    logout(){
+        console.log("you are logged out");
     }
-    intro(){
-        console.log(`My name is ${this.name}`);
+}
+user.login();
+//class is a  template of properties and methods
+//static  - common variables/mathods for class , accessed with classname to invoke
+
+class User{ //base clas or parent class or super class
+    static noOfUsers = 0;
+    constructor(name,age){
+        //instance variables
+        this.name=name;
+        this.age=age;
+        User.noOfUsers++;
+
+    }
+
+    login(){
+        console.log("Hi",this.name);
+        console.log("you are logged in");
+    }
+    logout(){
+        console.log("you are logged out");
+    }
+    static displayTotalUser(){
+        console.log("total number of users",User.noOfUsers);
+    }
+}
+let user1 = new User('sumi',20);
+let user2 = new User('ram',22);
+console.log(user1);
+console.log(user2);
+console.log(user1.age);
+// console.log("number of users",User.noOfUsers);
+User.displayTotalUser();
+user1.login();
+user2.logout();
+
+//inheritance
+class Userinfo extends User{//derived class or child class or sub class
+   constructor(name,age){
+    super(name,age)
+   
+   }
+    message(){
+        console.log('Details of : ',this.name)
+    }
+
+    //overriding - change the defining
+    login(){
+        console.log("Hi",this.name);
+        console.log("THank you");
+        return this
+    }
+}
+let userinfo = new Userinfo('sam');
+userinfo.message()
+userinfo.login()
+// user1.message()
+
+//method chaining
+userinfo.login().message()
+
+//encapsulation
+
+class Student{
+    constructor(){
+        let name,age;
+    }
+    getName(){
+        return this.name;
+    }
+    setName(name){
+        this.name= name;
+    }
+    getage(){
+        return this.age;
+    }
+
+    setage(age){
+        this.age= age;
     }
 
 }
 
-const samp= new valu('vinu');
-samp.intro();
+let stu = new Student();
+stu.setName('sumi');
+stu.setage(20);
+console.log(stu.getName(),stu.getage());
